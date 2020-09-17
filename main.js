5d425c84e505921ec262123733c86dc186b58005
tlfolkman <44623133+tlfolkman@users.noreply.github.com>
2020-09-16 20:23:46 -0600

    Edited Index
    Added a background and font styling

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var weeks = ["Week 1", "Week 2", "Week 3"];
 var links = ["notes.html", "challenges.html", "exercises.html"]
 weeks.forEach(myFunction);
-links.forEach(myFunction);
 
 function myFunction(weeks, links) {
   document.getElementById("list").innerHTML += "<a href=" + '"' + links + '"' + ">" + weeks + "</a>" + "<br>";
